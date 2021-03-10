e49a690daa552758854bf956bd82aaf12c0bc1dc
LoganKingEdu <king1303@umn.edu>
2021-03-10 00:57:32 -0600

    Fixed Queen movement bug

--- Pieces/Queen.java
+++ Pieces/Queen.java
@@ -7,7 +7,8 @@ import Game.Board;
 
 public class Queen extends Piece {
 
-    private int[] QUEEN_MOVE_OFFSETS = {-9, -8, -7, -1, 1, 7, 8, 9}; // directions queen can move in
+    private int[] QUEEN_DIAGONAL_MOVE_OFFSETS = {-9, -7, 7, 9}; // directions queen can move in
+    private int[] QUEEN_AXIAL_MOVE_OFFSETS = {-8, -1, 1, 8}; //separated by direction
 
     public Queen(Faction color, int coordinate){
         super(color, PieceType.QUEEN, coordinate, true);
@@ -25,16 +26,16 @@ public class Queen extends Piece {
     @Override
     public List<Integer> findLegalMoves(Board board) {
         List<Integer> legalMoves = new ArrayList<>();
-        for (int offset : QUEEN_MOVE_OFFSETS) {
+        for (int offset : QUEEN_DIAGONAL_MOVE_OFFSETS) {
             for (int i = 1; i <= 7; i++) {
                 int moveCoordinate = coordinate + (offset * i); //the coordinate we're checking
                 if(moveCoordinate < 0 || moveCoordinate > 63){ //ooBounds
-                    i = 8;
+                    i = 8;//stop looking in direction
                 }
                 else{
                     int columnOffset = Math.abs(board.getTiles()[coordinate].getColumn() - board.getTiles()[moveCoordinate].getColumn());
                     int rowOffset = Math.abs(board.getTiles()[coordinate].getRow() - board.getTiles()[moveCoordinate].getRow());
-                    if( (rowOffset == 0 || columnOffset == 0) || (columnOffset == rowOffset) ){
+                    if( (columnOffset == rowOffset) ){
                         if(board.getTiles()[moveCoordinate].isOccupied()){ //if there's a piece we cannot go farther
                             if(board.getTiles()[moveCoordinate].getPiece().getColor() != this.getColor()){//take a piece
                                 legalMoves.add(moveCoordinate);
@@ -42,6 +43,32 @@ public class Queen extends Piece {
                             i = 8;//stop looking in this direction
                         }
                         else{
+                            legalMoves.add(moveCoordinate);
+                        }                
+                    }
+                }
+            }
+        }
+        for (int offset : QUEEN_AXIAL_MOVE_OFFSETS) {
+            for (int i = 1; i <= 7; i++) {
+                int moveCoordinate = coordinate + (offset * i); //the coordinate we're checking
+                if(moveCoordinate < 0 || moveCoordinate > 63){ //ooBounds
+                    i = 8;//stop looking in direction
+                }
+                else{
+                    int columnOffset = Math.abs(board.getTiles()[coordinate].getColumn() - board.getTiles()[moveCoordinate].getColumn());
+                    int rowOffset = Math.abs(board.getTiles()[coordinate].getRow() - board.getTiles()[moveCoordinate].getRow());
+                    if( rowOffset == 0 || columnOffset == 0 ){
+                        if(board.getTiles()[moveCoordinate].isOccupied()){ //if there's a piece we cannot go farther
+                            if(board.getTiles()[moveCoordinate].getPiece().getColor() != this.getColor()){//take a piece
+                                if(!legalMoves.contains(moveCoordinate)){
+                                    legalMoves.add(moveCoordinate);
+                                }
+                                    
+                            }
+                            i = 8;//stop looking in this direction
+                        }
+                        else if(!legalMoves.contains(moveCoordinate)){
                             legalMoves.add(moveCoordinate);
                         }                
                     }
